5dd47bbe17fba863fac3dca474c647144a59d6fe
Decker W Brower <dckrbrower@gmail.com>
2014-07-08 18:24:02 -0600

    add Path/Pattern/Matcher modules

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,7 +20,8 @@ module.exports = function(grunt) {
              files: [
 //                "Gruntfile.js",
 //                 "PatternMatcher.js"
-                 "*.js"
+                 "*.js",
+                 "lib/**/*.js"
             ]
         },
         uglify: {
